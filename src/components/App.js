36f1b3598b30aba4f1c260784872198bde755ded
Pavvan Pradeep <pavvanpradeep@gmail.com>
2022-11-27 23:03:04 +0530

    version 1.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import Generate from './Generate'
+import Generate from './Generate'
 import {BrowserRouter as Router,Route, Routes } from 'react-router-dom'
 import Login from './Login'
 import Signup from './Signup'
@@ -7,14 +7,13 @@ import Signup from './Signup'
 export default function App() {
   return (
    <>
-   <Router>
-      <Routes>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
-      </Routes>
-   </Router>
-    {/* <Generate/> */}
-    
+      <Router>
+          <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/generate' element={<Generate/>}/>
+          </Routes>
+      </Router>   
    </>
   )
 }
